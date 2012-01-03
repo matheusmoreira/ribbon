09e202a720cfcb1e1058d50e10d34c7c930c6963
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-01-03 17:24:03 -0200

    Bump version to 0.4.0.

--- lib/ribbon/version.rb
+++ lib/ribbon/version.rb
@@ -11,12 +11,12 @@ class Ribbon < BasicObject
     # Minor version.
     #
     # Increments denote backward-compatible changes and additions.
-    MINOR = 3
+    MINOR = 4
 
     # Patch version.
     #
     # Increments denote changes in implementation.
-    PATCH = 1
+    PATCH = 0
 
     # Build version.
     #
