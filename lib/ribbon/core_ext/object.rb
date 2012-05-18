f2bdeee20fa3e5bcd6868f2cb91fcf60203c962c
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-05-18 15:05:39 -0300

    Add Object core extensions.

---
+++ lib/ribbon/core_ext/object.rb
@@ -0,0 +1,16 @@
+require 'ribbon/core_ext/basic_object'
+
+class Ribbon < BasicObject
+  module CoreExt
+
+    # Some useful methods.
+    module Object
+
+      alias yield_or_eval __yield_or_eval__
+
+    end
+
+    ::Object.send :include, ::Ribbon::CoreExt::Object
+
+  end
+end
