7c26480337518d65081c7702c84505d773b6b659
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-05-18 15:13:23 -0300

    Document __yield_or_eval__.

--- lib/ribbon/core_ext/basic_object.rb
+++ lib/ribbon/core_ext/basic_object.rb
@@ -4,6 +4,8 @@ class Ribbon < BasicObject
     # Some useful methods.
     module BasicObject
 
+      # Evaluates the block using +instance_eval+ if it takes no arguments;
+      # yields +self+ to it otherwise.
       def __yield_or_eval__(&block)
         if block.arity.zero? then instance_eval &block else block.call self end if block
       end
