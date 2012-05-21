85f1c42c127679aca530ec42da9a0aa0ec319518
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-05-21 18:09:36 -0300

    Add yard tags to CoreExtensions::BasicObject documentation

--- lib/ribbon/core_extensions/basic_object.rb
+++ lib/ribbon/core_extensions/basic_object.rb
@@ -9,6 +9,9 @@ class Ribbon < BasicObject
 
       # Evaluates the block using +instance_eval+ if it takes no arguments;
       # yields +self+ to it otherwise.
+      #
+      # @yieldparam [self] object this instance
+      # @return [Object, nil] the result of the block or nil if not given one
       def __yield_or_eval__(&block)
         if block.arity.zero? then instance_eval &block else block.call self end if block
       end
