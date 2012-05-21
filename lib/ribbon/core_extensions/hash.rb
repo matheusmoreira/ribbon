d1f1e60109a8195a7fe8180d02a4da904b09b019
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-05-21 18:02:15 -0300

    Add yard tags to CoreExtensions::Hash documentation

--- lib/ribbon/core_extensions/hash.rb
+++ lib/ribbon/core_extensions/hash.rb
@@ -10,19 +10,27 @@ class Ribbon < BasicObject
     module Hash
 
       # Converts this hash to a Ribbon.
+      #
+      # @return a new Ribbon with the contents of this hash
       def to_ribbon
         Ribbon.new self
       end
 
       # Converts this hash to a Ribbon::Wrapper.
+      #
+      # @return a new wrapped Ribbon with the contents of this hash
       def to_ribbon_wrapper
         Ribbon.wrap self
       end
 
       # Same as #to_ribbon.
+      #
+      # @return a new Ribbon with the contents of this hash
       alias to_rbon to_ribbon
 
       # Same as #to_ribbon_wrapper.
+      #
+      # @return a new wrapped Ribbon with the contents of this hash
       alias to_wrapped_ribbon to_ribbon_wrapper
 
     end
