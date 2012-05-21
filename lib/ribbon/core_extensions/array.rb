dbc524ff64dde5b175e2b535ab96036e2163f499
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-05-21 18:11:17 -0300

    Add yard tags to CoreExtensions::Array documentation

--- lib/ribbon/core_extensions/array.rb
+++ lib/ribbon/core_extensions/array.rb
@@ -11,6 +11,8 @@ class Ribbon < BasicObject
 
       # If the last argument is a hash, removes and converts it to a ribbon,
       # otherwise returns an empty ribbon.
+      #
+      # @return [Ribbon] the Ribbon at the end of this array
       def extract_ribbon!
         case last
           when Hash then Ribbon.new pop
@@ -22,14 +24,21 @@ class Ribbon < BasicObject
 
       # Extracts the last argument as a wrapped ribbon, or returns an empty one.
       # See #extract_ribbon! for details.
+      #
+      # @return [Ribbon::Wrapper] the wrapped Ribbon at the end of this array
       def extract_wrapped_ribbon!
         Ribbon.wrap extract_ribbon!
       end
 
       # Same as #extract_ribbon!
+      #
+      #
+      # @return [Ribbon] the Ribbon at the end of this array
       alias extract_options_as_ribbon! extract_ribbon!
 
       # Same as #extract_wrapped_ribbon!
+      #
+      # @return [Ribbon::Wrapper] the wrapped Ribbon at the end of this array
       alias extract_options_as_wrapped_ribbon! extract_wrapped_ribbon!
 
     end
